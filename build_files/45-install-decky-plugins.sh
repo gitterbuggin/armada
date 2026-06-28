6d055c8da30f70a8c589d68e6acbf12e3045db18
virtudude <virtudude@protonmail.com>
2026-06-27 20:54:12 -0400

    Revert back to using standard decky loader service

--- build_files/45-install-decky-plugins.sh
+++ build_files/45-install-decky-plugins.sh
@@ -18,6 +18,7 @@ decky_release="$(
 )"
 decky_version="$(jq -r '.tag_name' <<<"${decky_release}")"
 decky_url="$(jq -r '.assets[].browser_download_url | select(endswith("PluginLoader"))' <<<"${decky_release}")"
+decky_service_url=https://raw.githubusercontent.com/SteamDeckHomebrew/decky-loader/main/dist/plugin_loader-prerelease.service
 
 [[ -n "${decky_version}" && "${decky_version}" != "null" ]]
 [[ -n "${decky_url}" && "${decky_url}" != "null" ]]
@@ -26,6 +27,12 @@ install -d -m 0755 /usr/share/decky-loader
 curl --retry 3 --retry-delay 2 -fL -o /usr/share/decky-loader/PluginLoader "${decky_url}"
 chmod 0755 /usr/share/decky-loader/PluginLoader
 printf '%s\n' "${decky_version}" > /usr/share/decky-loader/.loader.version
+decky_service_tmp="$(mktemp)"
+curl --retry 3 --retry-delay 2 -fsSL "${decky_service_url}" |
+    sed 's#${HOMEBREW_FOLDER}#/var/home/armada/homebrew#g' \
+        >"${decky_service_tmp}"
+install -D -m 0644 "${decky_service_tmp}" /etc/systemd/system/plugin_loader.service
+rm -f "${decky_service_tmp}"
 
 systemctl enable armada-decky-sync.service
-systemctl enable armada-decky-loader.service
+systemctl enable plugin_loader.service
